9a51caa597e58071e5689b58fbc10fd5d8059a99
Mayank Anand <99637340+its-mayankanand@users.noreply.github.com>
2024-05-27 10:58:28 +0000

    config add.

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "mayank603@gmail.com"
+var accountPassword = "12345"
+accountCity = "Kolkata"
+let accountState;
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+console.table([accountEmail , accountId , accountPassword , accountCity , accountState])
+
+/*
+preferred not to use var 
+because of issue in block scope and functional scope
+*/
