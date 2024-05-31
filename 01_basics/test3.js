db1fa8641b4ad21bdb489a675b65a7519c69b87a
Mayank Anand <99637340+its-mayankanand@users.noreply.github.com>
2024-05-31 19:15:56 +0000

    config code.

--- 01_basics/test3.js
+++ 01_basics/test3.js
@@ -1,1 +1,20 @@
-console.log("Hello Mumma Papa")
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) //we are using nodejs, not browser
+
+//datatypes
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+//number = 2 to power 53
+//bigint
+//string = ""
+//boolean = true/false
+//null = standalone value
+//undefined = when initially not assigned value
+//symbol = unique
+//object 
+console.log(typeof("hitesh"))
+//In javascript null type is object which is wrong in its own way kind of error of language
+//And type of undefined is undefined but null is object.
