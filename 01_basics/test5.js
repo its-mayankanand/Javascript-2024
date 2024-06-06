1ad9dc65cd96c5ad78a9c9e2bda09daa8d522ab4
Mayank Anand <99637340+its-mayankanand@users.noreply.github.com>
2024-06-06 11:42:28 +0000

    Console push.

---
+++ 01_basics/test5.js
@@ -0,0 +1,40 @@
+//Outdated type
+// const name = "mayank"
+// const repoCount = 50
+
+// console.log(name + repoCount)
+
+//---------------------------------
+
+//Modern method
+// const name = "mayank"
+// const repoCount = 50
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+//this is known as string interpulation
+//----------------------------------
+
+const gameName = new String('Mayank')
+
+// console.log(gameName[0])
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(2))
+
+//console.log(gameName.indexOf('n'))
+
+//----------------------------------
+//To Divide it into a Substring
+
+// const meraName = new String('Anand-hc')
+
+// const newString = meraName.substring(0,4)
+
+// console.log(newString)
+
+//-----------------------------------
+
+
+
+
