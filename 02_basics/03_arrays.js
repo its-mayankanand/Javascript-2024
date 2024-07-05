e34d15bc3d95656acc6921716d21b09b7fda9a7e
Mayank Anand <99637340+its-mayankanand@users.noreply.github.com>
2024-07-05 12:13:40 +0000

    commit and push.

---
+++ 02_basics/03_arrays.js
@@ -0,0 +1,14 @@
+// some more array functions or methods
+
+// 1 . To make array from String
+console.log(Array.from("Hitesh"))
+
+// 2 . To check weather it is array or not
+console.log(Array.isArray("Hitesh"))
+
+// 3 . To make Array from Normal variable
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1,score2,score3))
+
