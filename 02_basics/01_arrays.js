1fdccea9a5ed9988012190ff7b82928b3c585741
Mayank Anand <99637340+its-mayankanand@users.noreply.github.com>
2024-07-05 06:23:10 +0000

    Console and push.

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,30 @@
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//Method 1 : 
+// const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+
+// const real_another_array = another_array.flat(Infinity)
+
+// console.log(real_another_array)
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//Method 2 : 
+
+const marvel_heros = ["thor","Ironman","Spiderman","monkey"]
+const dc_heros = ["superman","flash","batman","dog"]
+
+//push and concat array ke andar array dal dega
+
+//if using cancat then take another variable
+// const all_heros = marvel_heros.concat(dc_heros)
+// console.log(all_heros);
+
+
+//will use spread operator(...)
+// const all_new_heros = [...marvel_heros, ...dc_heros]
+// console.log(all_new_heros)
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+
