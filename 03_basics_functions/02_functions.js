b4d88828c3d41b6efe68b35f60182207b2af3cca
Mayank Anand <99637340+its-mayankanand@users.noreply.github.com>
2024-07-08 15:02:18 +0000

    commit and push.

---
+++ 03_basics_functions/02_functions.js
@@ -0,0 +1,54 @@
+//+++++++++++++++++++++++++++++++++++++++++++++++
+//Array relations functions 1:
+// const myNewArray = [200,400,100,600]
+
+// function returnSecondValue(getArray)
+// {
+//     return getArray[1]
+// }
+
+// console.log(returnSecondValue(myNewArray))
+
+//+++++++++++++++++++++++++++++++++++++++++++++++
+//1 ka other way:
+// const myNewArray = [200,400,100,600]
+// function returnSecondValue(getArray)
+// {
+//     return getArray[1]
+// }
+
+// console.log(returnSecondValue([200,400,500,1000]))
+//iska mtlb calling time me bhi array me pass 
+//kar skte hai
+//++++++++++++++++++++++++++++++++++++++++++++++++
+//Objects relations functions 2:
+
+// const user = {
+//     username:  "Mayank",
+//     price: 199
+// }
+
+// function handleObject(anyobject)
+// {
+//     console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+// }
+
+// handleObject(user)
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// 2: ka other way
+// function handleObject(anyobject)
+// {
+//     console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+// }
+
+// handleObject({
+//     username:"sam",
+//     price:399
+// })
+
+//iska mtlb calling time me bhi object banake
+//direct pass kar skte hai
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
