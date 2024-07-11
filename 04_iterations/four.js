ce878b7fcafcb72de910766cc768e0dc0941cc90
Mayank Anand <99637340+its-mayankanand@users.noreply.github.com>
2024-07-11 04:18:41 +0000

    push.

---
+++ 04_iterations/four.js
@@ -0,0 +1,13 @@
+// const coding = ["js","ruby","java"]
+
+// coding.forEach(function(val)
+// {
+//     console.log(val)
+// })
+//++++++++++++++++++++++++++++++++++++++++++++++
+
+const coding = ["js","ruby","java"]
+
+coding.forEach( (item) =>{
+    console.log(item)
+})
